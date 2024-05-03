ecabf2ad1930ddabeda8d8217cd4b6580e6e9361
Bruno Tenório <tenoriobn@gmail.com>
2024-05-03 07:08:02 -0300

    Testando um componente

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,4 +1,8 @@
-test("Primeiro teste", () => {
-  const numero = 10;
-  expect(numero).toBe(10);
+import {render, screen} from '@testing-library/react';
+import Cabecalho from './index'
+
+test('Deve renderizar o nome do usuário logado', () => {
+  render(<Cabecalho />)
+  const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
+  expect(nomeUsuario).toBeInTheDocument();
 })

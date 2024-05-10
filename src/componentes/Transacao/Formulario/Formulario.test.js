0552d364ac470e1d9b7bfafbdd1e3a879a62babd
Bruno Tenório <tenoriobn@gmail.com>
2024-05-10 07:16:18 -0300

    Simulando eventos

--- src/componentes/Transacao/Formulario/Formulario.test.js
+++ src/componentes/Transacao/Formulario/Formulario.test.js
@@ -25,3 +25,14 @@ describe('Deve renderizar um campo de input', () => {
     expect(campoTexto).toHaveValue(50);
   });
 });
+
+test('Deve chamar um evento de onSubmit ao clicar em realizar transação', () => { 
+  const realizarTransacao = jest.fn();
+
+  render(<Formulario realizarTransacao={realizarTransacao}/>);
+  const botao = screen.getByRole('button');
+
+  userEvent.click(botao);
+
+  expect(realizarTransacao).toHaveBeenCalledTimes(1);
+})
